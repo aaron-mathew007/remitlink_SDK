c8bdb84c7fa5850c54440b468c483204daa2ccbf
AARON MATHEW <aaronarackathara@gmail.com>
2025-02-10 22:26:18 +0530

    Create APIService.swift

---
+++ core/APIService.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+class APIService {
+    static let shared = APIService()
+    private let session: URLSession
+
+    private init() {
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = 30
+        session = URLSession(configuration: configuration)
+    }
+
+    func makeRequest(endpoint: String, method: String, headers: [String: String]?, body: [String: Any]?, completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let url = NetworkHelper.createURL(endpoint: endpoint) else {
+            completion(.failure(NSError(domain: "InvalidURL", code: -1, userInfo: nil)))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method
+        request.allHTTPHeaderFields = headers
+        
+        if let body = body {
+            do {
+                request.httpBody = try JSONSerialization.data(withJSONObject: body, options: [])
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            } catch {
+                completion(.failure(error))
+                return
+            }
+        }
+
+        session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(NSError(domain: "InvalidResponse", code: -2, userInfo: nil)))
+                return
+            }
+
+            guard (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NSError(domain: "HTTPError", code: httpResponse.statusCode, userInfo: nil)))
+                return
+            }
+
+            if let data = data {
+                completion(.success(data))
+            } else {
+                completion(.failure(NSError(domain: "NoData", code: -3, userInfo: nil)))
+            }
+        }.resume()
+    }
+}
