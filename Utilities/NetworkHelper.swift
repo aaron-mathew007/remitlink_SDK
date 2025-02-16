750e3ca0314870f8ec01c663c953686d891ae733
AARON MATHEW <aaronarackathara@gmail.com>
2025-02-16 15:15:23 +0530

    Update NetworkHelper.swift

--- Utilities/NetworkHelper.swift
+++ Utilities/NetworkHelper.swift
@@ -1,10 +1,9 @@
 import Foundation
 
 class NetworkHelper {
+    static let baseURL = "https://drap-sandbox.digitnine.com"
+
     static func createURL(endpoint: String) -> URL? {
-        guard let baseURL = URL(string: SDKConfig.shared.baseURL) else {
-            return nil
-        }
-        return baseURL.appendingPathComponent(endpoint)
+        return URL(string: baseURL + endpoint)
     }
 }
